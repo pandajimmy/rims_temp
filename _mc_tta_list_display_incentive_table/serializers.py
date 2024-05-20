c27b4b760eba121a9561a4c9b00c6b8207d304db
Vivian Quek Jia Yi <jyquek32@gmail.com>
2024-05-20 17:54:12 +0800

    Update serializers.py

--- _mc_tta_list_display_incentive_table/serializers.py
+++ _mc_tta_list_display_incentive_table/serializers.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 class TtaListDisplayIncentiveTableSerializer(serializers.ModelSerializer):
  #       subdept_key = SubdeptSerializer(many=True, read_only=True)
 
-    incentive_table = RimsCpSetBranchSerializer(read_only=True, source='branch_guid') 
+    branch = RimsCpSetBranchSerializer(read_only=True, source='branch_guid') 
 
     class Meta:
         model = TtaListDisplayIncentiveTable
