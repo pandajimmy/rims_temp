c2de72ccbd05250f3a08bf1fa73ec532f9757769
Vivian Quek Jia Yi <jyquek32@gmail.com>
2024-06-05 11:18:10 +0800

    display incentive check

--- _mc_tta_list_display_incentive_table/views.py
+++ _mc_tta_list_display_incentive_table/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from .models import TtaListDisplayIncentiveTable, CustomerProfile
+from _ml_rims_cp_set_branch.models import RimsCpSetBranch
 from .serializers import TtaListDisplayIncentiveTableSerializer
 from rest_framework.response import Response
 from rest_framework import status
@@ -21,6 +22,20 @@ class TtaListDisplayIncentiveTableViewSet(viewsets.ModelViewSet):
             return customer.customer_name
         except CustomerProfile.DoesNotExist:
             return 'Unknown'
+        
+    def get_branch_code(self, branch_guid):
+        try:
+            branch = RimsCpSetBranch.objects.get(branch_guid=branch_guid)
+            return branch.branch_code
+        except RimsCpSetBranch.DoesNotExist:
+            return 'Unknown'
+    
+    def get_branch_name(self, branch_guid):
+        try:
+            branch = RimsCpSetBranch.objects.get(branch_guid=branch_guid)
+            return branch.branch_name
+        except RimsCpSetBranch.DoesNotExist:
+            return 'Unknown'
 
     def create(self, request, *args, **kwargs):
         #group_details = request.data.get('group_details', {})
@@ -30,10 +45,13 @@ class TtaListDisplayIncentiveTableViewSet(viewsets.ModelViewSet):
         
         customer_guid = request.data.get('customer_guid')
         customer_name = self.get_customer_name(customer_guid)
+        branch_guid = request.data.get('branch_guid')
+        branch_code = self.get_branch_code(branch_guid)
+        branch_name = self.get_branch_name(branch_guid)
 
         data = request.data.copy()
-        #data['code'] = code
-        #data['outlet'] = outlet
+        data['code'] = branch_code
+        data['outlet'] = branch_name
         #data['percent'] = percent
         data['created_at'] = timezone.now().strftime('%Y-%m-%d %H:%M:%S')
         data['created_by'] = customer_name
